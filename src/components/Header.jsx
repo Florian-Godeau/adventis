776bfcd557ec7a7b2ccb792fe82d8b47232b6eb2
Florian-Godeau <flgodeau@gmail.com>
2024-03-26 15:33:58 +0100

    ajout du header test

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+function Header() {
+  return (
+    <Navbar className="navbar-opacity" bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">MonSite</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Accueil</Nav.Link>
+            <Nav.Link href="#link">Lien</Nav.Link>
+            {/* Ajoutez d'autres liens de navigation ici */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
+
