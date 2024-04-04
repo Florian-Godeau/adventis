79b5e6daf5eeeccef1d9468866e08bd035b19080
Florian-Godeau <flgodeau@gmail.com>
2024-04-04 17:04:28 +0200

    ajustement header et import fontawesome

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
-function Header() {
+function Header({ onNavigate }) {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar className="navbar-opacity" bg="white" variant="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">MonSite</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Accueil</Nav.Link>
-            <Nav.Link href="#link">Lien</Nav.Link>
-            {/* Ajoutez d'autres liens de navigation ici */}
+    <Navbar className="navbar-opacity custom-navbar" bg="white" variant="light" expand="lg" expanded={expanded} onToggle={() => setExpanded(!expanded)}>
+      <Container className="d-flex justify-content-center justify-content-lg-between">
+        <Nav className="me-auto order-1">
+          <Nav.Link href="https://www.instagram.com/adven_tis" target="_blank" className="instagram-link">
+            <FontAwesomeIcon icon={faInstagram} /> {/* Utilisation de l'icône Instagram */}
+          </Nav.Link>
+        </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-auto order-2" />
+        <Navbar.Brand href="#" onClick={() => onNavigate(1)} className="mx-auto order-lg-1 artistName">ADVENTIS</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav" className="order-3">
+          <Nav className="ms-auto">
+            <Nav.Link onClick={() => onNavigate(1)}>Accueil</Nav.Link>
+            <Nav.Link onClick={() => onNavigate(2)}>À propos</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -20,4 +28,3 @@ function Header() {
 }
 
 export default Header;
-
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactFullpage from '@fullpage/react-fullpage';
 import Header from "../components/Header";
 import Slider from "../components/Slider";
 import About from "../components/About";
 
-const Home = () => (
-  <>
-    <Header />
-    <ReactFullpage
-      licenseKey={'YOUR_LICENSE_KEY'} // Remplacez par votre clé de licence si nécessaire
-      scrollingSpeed={700}
-      navigation={true} // Active les bulletpoints de navigation
-      navigationPosition={'right'} // Positionne les bulletpoints à droite
-      sectionsColor={['#ff5f45', '#0798ec']} // Exemple de couleurs de fond pour chaque section
-      render={({ state, fullpageApi }) => {
-        return (
-          <ReactFullpage.Wrapper>
-            <div className="section"><Slider /></div>
-            <div className="section"><About /></div>
-          </ReactFullpage.Wrapper>
-        );
-      }}
-    />
-  </>
-);
+class Home extends Component {
+  fullpageApi = null;
+
+  onNavigate = (destination) => {
+    if (this.fullpageApi) {
+      this.fullpageApi.moveTo(destination);
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <Header onNavigate={this.onNavigate} />
+        <ReactFullpage
+          licenseKey={'YOUR_LICENSE_KEY'}
+          scrollingSpeed={700}
+          navigation={true}
+          navigationPosition={'right'}
+          sectionsColor={['#ff5f45', '#0798ec']}
+          render={({ state, fullpageApi }) => {
+            // Stocker la référence de fullpageApi dans l'instance du composant
+            this.fullpageApi = fullpageApi;
+
+            return (
+              <ReactFullpage.Wrapper>
+                <div className="section"><Slider /></div>
+                <div className="section"><About /></div>
+              </ReactFullpage.Wrapper>
+            );
+          }}
+        />
+      </>
+    );
+  }
+}
 
 export default Home;
-
-
