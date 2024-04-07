50e9292e05d6e64aa6790d121ec9dd0e2ed092e3
Florian-Godeau <flgodeau@gmail.com>
2024-04-07 18:15:41 +0200

    ajout bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const About = () => {
   return (
     <div className="about-background">
-      <h1>Bienvenue sur la page À Propos</h1>
+      <div className="text-card">
+        <p className='defTitle'>Adventice:</p>
+        <p className='def'>Nom scientifique que l'on donne aux plantes indésirables du jardin, à toutes celles qui poussent d'elles mêmes, à l'état brut, spontané.</p>
+        <p className='bio'>Eva Husson, alias Adventis, est une jeune artiste plasticienne de 23 ans. 
+                           Autodidacte, son univers gravite autour du portrait qu'elle fais évoluée au fil des années et de ses inspirations. 
+                           Elle travaille essentiellement la matière; tissus, cartons, plastique, etc. Cela lui permet une recherche en constante évolution, 
+                           un puit sans fond d'inspirations pour faire évoluer et complexifier ses travaux.
+        </p>
+      </div>
     </div>
   );
 };
